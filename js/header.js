2fd9937835754e8c9c05f7b827123c6c6e531b33
orabampho <orabampho@outlook.com>
2022-03-01 20:58:17 -0500

    add style

--- js/header.js
+++ js/header.js
@@ -10,12 +10,12 @@ class Header extends HTMLElement {
           <p>Rabampho</p>
         </div>
         <ul>
-          <li><a href="/index.html" class="active">HOME</a></li>
-          <li><a href="/projects.html">Projects</a></li>
-          <li><a href="/api-unit-tests.html">Api-Unit-Tests</a></li>
-          <li><a href="/database-diagram.html">Database-Diagrams</a></li>
-          <li><a href="/resume.html">Resume</a></li>
-          <li><a href="/about.html">About Me</a></li>
+          <li><a href="index.html" class="active">HOME</a></li>
+          <li><a href="projects.html">Projects</a></li>
+          <li><a href="api-unit-tests.html">Api-Unit-Tests</a></li>
+          <li><a href="database-diagrams.html">Database-Diagrams</a></li>
+          <li><a href="resume.html">Resume</a></li>
+          <li><a href="about.html">About Me</a></li>
         </ul>
       </nav>
       `;
